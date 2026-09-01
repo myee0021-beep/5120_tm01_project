123ec4fac7afcf475bca33825d0283c382bed955
myee0021-beep <myee0021@student.monash.edu>
2026-09-01 17:52:07 +0800

    Load verified species state occurrence data

--- src/worker-step3.js
+++ src/worker-step3.js
@@ -86,7 +86,7 @@ export default {
 
     updatedHtml = updatedHtml.replace(
       /<\/body>/i,
-      '<script src="/iteration1-fixes.js"></script><script src="/bilingual-fallback.js"></script><script src="/provenance-links.js"></script><script src="/hide-prevention-source-summary.js"></script><script src="/state-occurrence-data.js"></script></body>'
+      '<script src="/iteration1-fixes.js"></script><script src="/bilingual-fallback.js"></script><script src="/provenance-links.js"></script><script src="/hide-prevention-source-summary.js"></script><script src="/state-occurrence-data.js"></script><script src="/species-state-occurrence.js"></script></body>'
     );
 
     const headers = new Headers(response.headers);
